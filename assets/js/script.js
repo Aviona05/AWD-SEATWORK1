0d66fd1027d87515965f00dfde3fc5528e96cc43
Aviona05 <202312248@fit.edu.ph>
2025-01-29 16:52:01 +0800

    refactor(login page): added logo, background, make button functioning by using js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function login() {
+  const password = document.getElementById('password').value;
+  const correctPassword = "1234";
+
+  if (password === correctPassword) {
+    alert("Login successful!");
+    window.location.href = "../../pages/Dashboard/index.html";
+  } else {
+    alert("Incorrect password. Please try again.");
+  }
+}
